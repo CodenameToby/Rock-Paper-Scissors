0e4ca5aae3150ff2e9919966bb097966022be84a
Toby Warburton <tobywarburton10@gmail.com>
2023-05-25 19:32:29 -0700

    Initial set up, add index.html and script

---
+++ script.js
@@ -0,0 +1,24 @@
+// objects the user will be prompted to chose
+// loop to run based on each potential user choice
+
+window.alert("Do you want to play?") 
+var rps = ["rock", "paper", "scissors"]
+var userChoice = window.prompt("Enter rock, paper or scissors.")
+userChoice = userChoice.toLowerCase;
+console.log(userChoice)
+
+
+function gamePlay( ){
+    if (rps == "userChoice") {
+        
+    } elseif (rps == "userChoice"){
+        
+    } elseif (rps == "userChoice"){
+
+    } else {
+        console.log()
+    }
+
+}
+
+gamePlay
